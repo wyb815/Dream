3c788aa64f9ffc737679a53cb453409a5eea31bb
wyb815 <87474581@qq.com>
2026-04-19 23:47:10 +0800

    feat: 完善气球逻辑

--- game/objs/balloon/Balloon.gd
+++ game/objs/balloon/Balloon.gd
@@ -5,13 +5,16 @@ export(NodePath) var balloon_end
 var catched_by_area: Area2D
 var move_speed := 200.0
 var vel := Vector2()
-var end_pos := Vector2()
+var end_pos: Vector2
+var start_pos: Vector2
+var is_start_move := false
 
 onready var handle := $handle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_priority(ProcessPriority.BALLON)
+	start_pos = position
 	if balloon_end:
 		end_pos = (get_node(balloon_end) as Node2D).global_position
 
@@ -19,25 +22,31 @@ func _physics_process(delta):
 	if not balloon_end:
 		return
 		
+	if not is_start_move:
+		return
+		
+	var target_pos = get_parent().to_local(end_pos)
+			
+	if target_pos == position:
+		if catched_by_area:
+			catched_by_area.get_parent().is_follow = false;
+			catched_by_area = null
+		position = start_pos
+		is_start_move = false
+		return
+
+	var calc_pos = position.move_toward(target_pos, move_speed * delta)
+	var move_offset = target_pos - position
+	var final_vel;
+
+	if calc_pos != end_pos:
+		final_vel = move_and_slide(move_offset.normalized() * move_speed, Vector2.UP)
+	else:
+		# 到达目的地
+		final_vel = move_and_slide(move_offset / delta, Vector2.UP)
+		
 	if catched_by_area:
 		if catched_by_area.get_parent().is_follow:
-			var target_pos = get_parent().to_local(end_pos)
-			
-			if target_pos == position:
-				catched_by_area.get_parent().is_follow = false;
-				catched_by_area = null
-				return
-			
-			var calc_pos = position.move_toward(target_pos, move_speed * delta)
-			var move_offset = target_pos - position
-			var final_vel;
-			
-			if calc_pos != end_pos:
-				final_vel = move_and_slide(move_offset.normalized() * move_speed, Vector2.UP)
-			else:
-				# 到达目的地
-				final_vel = move_and_slide(move_offset / delta, Vector2.UP)
-			
 			# 拖着走
 			catched_by_area.get_parent().follow_v = final_vel
 		else:
@@ -52,6 +61,7 @@ func _on_Area2D_area_entered(area: Area2D):
 		catched_by_area = area
 		catched_by_area.get_parent().is_follow = true
 		_fix_body_pos()
+		is_start_move = true
 
 func _on_Area2D_area_exited(area: Area2D):
 	if catched_by_area == area:
