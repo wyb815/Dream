88bb6954550017c476c37b18a6841591e0692904
wyb815 <87474581@qq.com>
2026-04-22 13:12:42 +0800

    feat: 小跳跃支持

--- game/objs/player/Player.gd
+++ game/objs/player/Player.gd
@@ -10,6 +10,7 @@ var height := 150
 var is_follow := false
 var follow_v := Vector2(0, 0)
 var is_on_platform := false
+var jump_apply_left_time := 0.0
 
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 onready var move_body = $move
@@ -22,8 +23,21 @@ func _physics_process(delta: float) -> void:
 		
 	velocity.y += gravity * delta
 	
+	var isJumpPressed = Input.is_action_pressed("ui_accept");
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		jump_apply_left_time = 0.5
 		velocity.y = JUMP_VELOCITY * up_dir.y
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			jump_apply_left_time -= delta
+		else:
+			if jump_apply_left_time > 0:
+				#松开跳跃键，跳的近一些
+				velocity.y *= 0.3
+				jump_apply_left_time = 0
+			
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
